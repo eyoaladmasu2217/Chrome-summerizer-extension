69b3af3a2292d439d597950dcb9b6f0bc8113bd8
eyoaladmasu2217 <eyoaladmasu@gmail.com>
2026-03-06 05:29:07 -0800

    feat: introduce centralized constants module

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+// Centralized constants for the extension
+
+export const DEFAULT_MODEL = 'Summerizer';
+export const MIN_SELECTION_LENGTH = 50;
+export const OVERLAY_Z_INDEX = 2147483647;
+
+export const STORAGE_KEYS = {
+    SUMMARY_LENGTH: 'summaryLength',
+    OUTPUT_LANGUAGE: 'outputLanguage',
+    AI_MODEL: 'aiModel',
+    API_KEY: 'apiKey',
+    DARK_MODE: 'darkMode',
+    AUTO_COPY: 'autoCopy'
+};
